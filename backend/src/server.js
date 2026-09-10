0ea40a599a623ad8472160f888d378b466fedb0b
Thomas Centurión <tcenturion.dev@gmail.com>
2026-09-10 01:09:01 -0300

    Hice cambios para preparar el deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,52 +1,19 @@
 import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./config/database.config.js";
-import User from "./dao/models/user.model.js";
-import bcrypt from "bcrypt";
 
 const PORT = process.env.PORT || 3000;
 
-const createAdminUser = async () => {
-  try {
-    const existingAdmin = await User.findOne({
-      username: process.env.DEMO_USERNAME,
-    });
-
-    if (existingAdmin) {
-      if (existingAdmin.role !== "demo") {
-        existingAdmin.role = "demo";
-        await existingAdmin.save();
-        }
-        
-      return;
-    }
-
-    const hashedPassword = await bcrypt.hash(
-      process.env.DEMO_PASSWORD,
-      10
-    );
-
-    await User.create({
-      name: "Administrador Demo",
-      username: process.env.DEMO_USERNAME,
-      password: hashedPassword,
-      role: "demo",
-    });
-
-    console.log("Usuario demo creado correctamente");
-  } catch (error) {
-    console.error("Error al crear usuario demo:", error.message);
-  }
-};
-
 const startServer = async () => {
   await connectDB();
-
-  await createAdminUser();
 
   app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
   });
 };
 
-startServer();
+if (!process.env.VERCEL) {
+  startServer();
+}
+
+export default app;
